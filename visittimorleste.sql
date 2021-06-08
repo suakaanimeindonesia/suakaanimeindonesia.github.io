98103cf298f5e9c059a2e6a9c5af5df0d3ebf2ef
Padang Perwira Yudha <terrificsubs@gmail.com>
2021-06-08 16:46:13 +0700

    Add files via upload

--- visittimorleste.sql
+++ visittimorleste.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jun 2021 pada 05.58
--- Versi server: 10.4.18-MariaDB
--- Versi PHP: 8.0.3
+-- Generation Time: Jun 08, 2021 at 11:47 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -34,7 +34,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id_category`, `category_name`, `image`) VALUES
@@ -50,7 +50,7 @@ INSERT INTO `category` (`id_category`, `category_name`, `image`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `localreview`
+-- Table structure for table `localreview`
 --
 
 CREATE TABLE `localreview` (
@@ -62,10 +62,17 @@ CREATE TABLE `localreview` (
   `specialtip` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `localreview`
+--
+
+INSERT INTO `localreview` (`id_localreview`, `id_tours`, `id_user`, `quote`, `whyshouldvisit`, `specialtip`) VALUES
+(2, 2, 3, 'Nikmati rasa stroberi di sini', 'Enak banget buat duduk duduk gaes', 'Mending kalau ke sini bawa golok buat bacok pengunjung sekitar');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `precinct`
+-- Table structure for table `precinct`
 --
 
 CREATE TABLE `precinct` (
@@ -79,7 +86,7 @@ CREATE TABLE `precinct` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `precinct_tours`
+-- Table structure for table `precinct_tours`
 --
 
 CREATE TABLE `precinct_tours` (
@@ -90,7 +97,7 @@ CREATE TABLE `precinct_tours` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `promotions`
+-- Table structure for table `promotions`
 --
 
 CREATE TABLE `promotions` (
@@ -108,7 +115,7 @@ CREATE TABLE `promotions` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `schedule`
+-- Table structure for table `schedule`
 --
 
 CREATE TABLE `schedule` (
@@ -119,7 +126,7 @@ CREATE TABLE `schedule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `schedule`
+-- Dumping data for table `schedule`
 --
 
 INSERT INTO `schedule` (`days`, `from_time`, `to_time`, `id_tours`) VALUES
@@ -133,7 +140,7 @@ INSERT INTO `schedule` (`days`, `from_time`, `to_time`, `id_tours`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `spotlights`
+-- Table structure for table `spotlights`
 --
 
 CREATE TABLE `spotlights` (
@@ -141,13 +148,21 @@ CREATE TABLE `spotlights` (
   `spotlights_title` varchar(255) NOT NULL,
   `id_tours` int(11) NOT NULL,
   `date_posted` datetime NOT NULL,
+  `spotlights_content` text NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `spotlights`
+--
+
+INSERT INTO `spotlights` (`id_spotlights`, `spotlights_title`, `id_tours`, `date_posted`, `spotlights_content`, `image`) VALUES
+(4, 'sadsad', 1, '2021-06-08 15:59:59', '<b>dafdafsfd</b>', 'free_PNG90771.png-1623142799502-68107490.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tours`
+-- Table structure for table `tours`
 --
 
 CREATE TABLE `tours` (
@@ -162,7 +177,7 @@ CREATE TABLE `tours` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tours`
+-- Dumping data for table `tours`
 --
 
 INSERT INTO `tours` (`id_tours`, `id_category`, `name`, `address`, `website`, `phone`, `description`, `image`) VALUES
@@ -170,12 +185,12 @@ INSERT INTO `tours` (`id_tours`, `id_category`, `name`, `address`, `website`, `p
 (2, 1, 'satu', 'satu', 'satu', 'satu', 'satu', '405ff63f-2ac8-473e-807e-495f21850af9.jpg'),
 (3, 1, 'xxx', 'xxx', 'xxx', 'xxx', 'xxx', '405ff63f-2ac8-473e-807e-495f21850af9.jpg'),
 (5, 1, 'Memek', 'awaw', 'awaw', 'aw', 'awaw', '02a54dee1a0b584d33e37c89a80aad68.jpg-1623085069886-915700970.jpg'),
-(6, 9, 'nyiahaha', 'nyiahaha', 'nyiahaha', 'nyiahaha', 'nyiahaha', 'sneakpeak1.png-1623087855205-688261418.png');
+(6, 9, 'tes123', 'nyiahaha', 'nyiahaha', 'nyiahaha', 'nyiahaha', 'sneakpeak1.png-1623087855205-688261418.png');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -189,17 +204,26 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `name`, `type`, `minitype`, `aboutme`, `locals`, `avatar`) VALUES
+(1, 'padang', 'Explorer', 'Foodies | Wibu | Ginseng', 'ganteng orangnya', 'No', 'avatarpadang.jpg'),
+(3, 'ganteng123', 'Explorer', 'a | b', 'guanteng pollzzzzz', 'No', 'Screenshot_1618976987.png-1623140463340-265041052.png'),
+(4, 'User 5', 'Tourist', 'toursit | ganteng', 'Toursit ghanteng', 'Yes', 'unnamed.png-1623139021776-797246257.png');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id_category`);
 
 --
--- Indeks untuk tabel `localreview`
+-- Indexes for table `localreview`
 --
 ALTER TABLE `localreview`
   ADD PRIMARY KEY (`id_localreview`),
@@ -207,135 +231,135 @@ ALTER TABLE `localreview`
   ADD KEY `id_user` (`id_user`);
 
 --
--- Indeks untuk tabel `precinct`
+-- Indexes for table `precinct`
 --
 ALTER TABLE `precinct`
   ADD PRIMARY KEY (`id_precinct`);
 
 --
--- Indeks untuk tabel `precinct_tours`
+-- Indexes for table `precinct_tours`
 --
 ALTER TABLE `precinct_tours`
   ADD KEY `id_precinct` (`id_precinct`),
   ADD KEY `id_tours` (`id_tours`);
 
 --
--- Indeks untuk tabel `promotions`
+-- Indexes for table `promotions`
 --
 ALTER TABLE `promotions`
   ADD PRIMARY KEY (`id_promotions`),
   ADD KEY `id_tours` (`id_tours`);
 
 --
--- Indeks untuk tabel `schedule`
+-- Indexes for table `schedule`
 --
 ALTER TABLE `schedule`
   ADD KEY `id_tours` (`id_tours`);
 
 --
--- Indeks untuk tabel `spotlights`
+-- Indexes for table `spotlights`
 --
 ALTER TABLE `spotlights`
   ADD PRIMARY KEY (`id_spotlights`),
   ADD KEY `id_tours` (`id_tours`);
 
 --
--- Indeks untuk tabel `tours`
+-- Indexes for table `tours`
 --
 ALTER TABLE `tours`
   ADD PRIMARY KEY (`id_tours`),
   ADD KEY `id_category` (`id_category`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `localreview`
+-- AUTO_INCREMENT for table `localreview`
 --
 ALTER TABLE `localreview`
-  MODIFY `id_localreview` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_localreview` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `precinct`
+-- AUTO_INCREMENT for table `precinct`
 --
 ALTER TABLE `precinct`
   MODIFY `id_precinct` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `promotions`
+-- AUTO_INCREMENT for table `promotions`
 --
 ALTER TABLE `promotions`
   MODIFY `id_promotions` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `spotlights`
+-- AUTO_INCREMENT for table `spotlights`
 --
 ALTER TABLE `spotlights`
-  MODIFY `id_spotlights` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_spotlights` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `tours`
+-- AUTO_INCREMENT for table `tours`
 --
 ALTER TABLE `tours`
   MODIFY `id_tours` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `localreview`
+-- Constraints for table `localreview`
 --
 ALTER TABLE `localreview`
   ADD CONSTRAINT `localreview_ibfk_1` FOREIGN KEY (`id_tours`) REFERENCES `tours` (`id_tours`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `localreview_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `precinct_tours`
+-- Constraints for table `precinct_tours`
 --
 ALTER TABLE `precinct_tours`
   ADD CONSTRAINT `precinct_tours_ibfk_1` FOREIGN KEY (`id_precinct`) REFERENCES `precinct` (`id_precinct`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `precinct_tours_ibfk_2` FOREIGN KEY (`id_tours`) REFERENCES `tours` (`id_tours`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `promotions`
+-- Constraints for table `promotions`
 --
 ALTER TABLE `promotions`
   ADD CONSTRAINT `promotions_ibfk_1` FOREIGN KEY (`id_tours`) REFERENCES `tours` (`id_tours`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `schedule`
+-- Constraints for table `schedule`
 --
 ALTER TABLE `schedule`
   ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`id_tours`) REFERENCES `tours` (`id_tours`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `spotlights`
+-- Constraints for table `spotlights`
 --
 ALTER TABLE `spotlights`
   ADD CONSTRAINT `spotlights_ibfk_1` FOREIGN KEY (`id_tours`) REFERENCES `tours` (`id_tours`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `tours`
+-- Constraints for table `tours`
 --
 ALTER TABLE `tours`
   ADD CONSTRAINT `tours_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE;
